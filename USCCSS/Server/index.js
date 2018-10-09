a1f60862ceb49e5a8668c104c760ac8d6715f342
Ayse Tuba <tubavardan@gmail.com>
2018-10-09 16:45:56 -0500

    last

--- USCCSS/Server/index.js
+++ USCCSS/Server/index.js
@@ -34,5 +34,5 @@ var config = require('./config/config');
 var port = process.env.port || 3000
 require('./config/express')(app, config);
 require('http').createServer(app).listen(port, function () {
-console.log("HTTP Server listening on port: %d, in %s mode", port, app.get('env'));
+console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
